bb57cd6664d0366965978d6332d53e381e21c6ec
Alexia Helper <110706851+AlexiaCatanzaro@users.noreply.github.com>
2022-10-04 20:25:38 -0300

    Add files via upload

---
+++ Agenda4.Rec.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.agenda4recuperacao;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexia
+ */
+public class NewMain1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double dias;
+         double quilometros ;
+         double carro1;
+        /*        double aluguel carro1 ;*/        double totaldias ;
+         double totalquilometros ;
+         Scanner ler = new scanner (System.in) ;
+         carro1 = ler.nextDouble () ;
+        
+         System.out.printIn (Digite o numero do carro) ;
+         carro1 = ler.nextDouble ();
+         System.out.println (); (Digite o total de dias) ;
+         dias = ler.nextDouble () ;
+         System.out.println(Digite os quilometros a serem rodados);
+         quilometros = ler.nextDouble () ;
+         totaldias = dias*120;
+         totalquilometros = quilometros*0,79;
+         aluguelcarro1 = totaldias + totalquilometros;
+         
+         double dias;
+         double quilometros ;
+         double carro2;
+        /*        double aluguel carro2 ;*/        double totaldias ;
+         double totalquilometros ;
+         Scanner ler = new scanner (System.in) ;
+         carro1 = ler.nextDouble () ;
+        
+         System.out.printIn (Digite o numero do carro) ;
+         carro1 = ler.nextDouble ();
+         System.out.println (); (Digite o total de dias) ;
+         dias = ler.nextDouble () ;
+         System.out.println(Digite os quilometros a serem rodados);
+         quilometros = ler.nextDouble () ;
+         totaldias = dias*140
+         totalquilometros = quilometros*0,79
+         aluguelcarro1 = totaldias + totalquilometros
+                 
+         double dias;
+         double quilometros ;
+         double carro3;
+        /*        double aluguel carro3 ;*/        double totaldias ;
+         double totalquilometros ;
+         Scanner ler = new scanner (System.in) ;
+         carro1 = ler.nextDouble () ;
+        
+         System.out.printIn (Digite o numero do carro) ;
+         carro1 = ler.nextDouble ();
+         System.out.println (); (Digite o total de dias) ;
+         dias = ler.nextDouble () ;
+         System.out.println(Digite os quilometros a serem rodados);
+         quilometros = ler.nextDouble () ;
+         totaldias = dias*160
+         totalquilometros = quilometros*0,79
+         aluguelcarro1 = totaldias + totalquilometros        
+    }
+    
+}
